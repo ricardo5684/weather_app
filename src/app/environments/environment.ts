d0537cd98d760565d7f6a43dfe6a542b9c2c67a4
ricardo5684 <12.2019.minyetun@gmail.com>
2023-12-15 23:25:03 +0630

    first commit

---
+++ src/app/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  weatherApiBaseUrl: 'https://weatherapi-com.p.rapidapi.com/current.json?q=53.1%2C-0.13',
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue:'weatherapi-com.p.rapidapi.com',
+  XRapidAPIKeyHeaderName:'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue:'59d1492bf7msh097d5dec5d3b28fp1f821bjsn5eb1e6ccc992',
+};
